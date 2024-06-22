85a412440592a276cf888ba15151d0795d0738ae
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-06-22 18:37:39 +0600

    added tabs

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
+
 import Carousel from '../../Components/Carousel/Carousel';
+import TheTabs from '../../Components/Tabs/Tabs';
+
 
 const Home = () => {
   return (
     <div>
       <Carousel></Carousel>
+      <TheTabs></TheTabs>
+       
     </div>
   );
 };
